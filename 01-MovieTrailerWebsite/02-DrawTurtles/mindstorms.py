e34906cfc396063e3494145f2aac00a858a78db1
dgabel001 <duane_gabel@cable.comcast.com>
2016-08-20 07:33:50 -0600

    Added turtle classes

---
+++ 01-MovieTrailerWebsite/02-DrawTurtles/mindstorms.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("red")
+
+def draw_shapes():
+  draw_square()
+  draw_circle()
+  draw_triangle()
+
+def move(turtle_name, length, angle):
+  turtle_name.forward(length)
+  turtle_name.right(angle)
+ 
+def draw_square():
+  brad = turtle.Turtle()
+  brad.shape("turtle")
+  brad.speed(1)
+  brad.color("blue")
+
+  counter = 0
+  while (counter < 4):
+    move(brad, 100, 90)
+    counter += 1
+
+def draw_circle():
+  angie = turtle.Turtle()
+
+  angie.circle(100)
+
+def draw_triangle():
+  homer = turtle.Turtle()
+
+  counter = 0
+  while (counter < 3):
+    move(homer, 200, 120)
+    counter += 1
+
+draw_shapes()
+
+window.exitonclick()
